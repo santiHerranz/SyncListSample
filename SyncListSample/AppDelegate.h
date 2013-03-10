9f68f104f32a78f6c26ca44c9b816af19182bae6
santiherranz <santi.herranz@gmail.com>
2013-03-10 01:11:28 +0100

    Initial Commit

---
+++ SyncListSample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SyncListSample
+//
+//  Created by Santi on 10/03/13.
+//  Copyright (c) 2013 santiHerranz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
