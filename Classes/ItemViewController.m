2588d758f4155fcc4e2552e782d202121dd70ba0
santiherranz <santi.herranz@gmail.com>
2013-03-13 00:24:11 +0100

    Disable cache of FechedResultController

--- Classes/ItemViewController.m
+++ Classes/ItemViewController.m
@@ -156,7 +156,7 @@
     
     // Edit the section name key path and cache name if appropriate.
     // nil for section name key path means "no sections".
-    NSFetchedResultsController *aFetchedResultsController = [[NSFetchedResultsController alloc] initWithFetchRequest:fetchRequest managedObjectContext:managedObjectContext sectionNameKeyPath:nil cacheName:@"Root"];
+    NSFetchedResultsController *aFetchedResultsController = [[NSFetchedResultsController alloc] initWithFetchRequest:fetchRequest managedObjectContext:managedObjectContext sectionNameKeyPath:nil cacheName:nil]; //cacheName:@"Root"
     aFetchedResultsController.delegate = self;
     self.fetchedResultsController = aFetchedResultsController;
     
